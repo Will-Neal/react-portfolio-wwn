c9fc21e05cdb71c20e0c70d56c12994211912913
Will Neal <willwneal@gmail.com>
2022-03-24 19:48:14 -0400

    Added NavBar and Routes, fixed all associated styling, just missing icons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import Intro from './components/intro/intro';
-import ProjectList from './components/projectList/projectList'
-import Contact from './components/contact/contact'
+import Header from './pages/navbar/header';
 // import Project from './components/projects/project';
 
 function App() {
   return (
     <div>
-      <Intro/>
-      <ProjectList/>
-      <Contact/>
+      <Header/>
     </div>
   );
 }
---
+++ src/pages/navbar/header.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './header.css'
+import Intro from '../../components/intro/intro'
+import ProjectList from '../../components/projectList/projectList';
+import Contact from '../../components/contact/contact';
+
+const Header = () => {
+    return (
+        <Router>
+            <div className='main'>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className='nav-wrapper'>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">Main</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/projects">Projects</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Intro />}
+                    />
+                    <Route 
+                        path="/projects"
+                        element={<ProjectList />}
+                    />
+                    <Route
+                        path="/contact"
+                        element={<Contact />}
+                    />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
+
+export default Header
