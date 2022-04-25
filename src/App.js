1c200bf193771a019bbcafc4edf3d19c93c6c477
Will Neal <willwneal@gmail.com>
2022-04-25 12:54:42 -0400

    Added own favicon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-// import logo from './logo.svg';
-import './App.css';
-import Contact from './components/contact/contact';
+// import './App.css';
 import Header from './pages/navbar/header';
-// import Project from './components/projects/project';
 
 function App() {
   return (
